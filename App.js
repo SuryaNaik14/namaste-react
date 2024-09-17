def52f4309f9a313989204395c6102f34dd69dc1
Surya Naik <suryakanthnaik01@gmail.com>
2024-09-17 13:20:50 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,52 +1,6 @@
-// This is used to create 1 heading tag...
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const heading = React.createElement("h1", {id:"heading"},"Hello world in React...!");
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-// -----------------------------------------------------------------------------
-
-// i need to create the html tag that div inside the div means nested div in react....
-
-// const parent = React.createElement(
-//     "div" , 
-//     {id : "parent"},
-//     React.createElement(
-//         "div",
-//         {id : " child"},
-//         React.createElement("h1",{}," iam heading tag 1")
-//         )
-//     );
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(parent);
-
-// ----------------------------------------------------------------------------
-
-// creating the html tag with siblings of the h1 tag by using react
-
-// const parent = React.createElement(
-//     "div", 
-//     {id:"parent"},
-//     React.createElement(
-//         "div", 
-//         {id:"child"},[
-//         React.createElement("h1", {},"Hello i am h1 tag"),
-//         React.createElement("h2", {},"Hello i am h2 tag")
-//         ]
-//         ));
-
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(parent);
-
-// -----------------------------------------------------------------------------------
-
-// creating the html tag by using the react like div parent one heading div child one heading tag.
 
 const parent = React.createElement(
     "div", 
